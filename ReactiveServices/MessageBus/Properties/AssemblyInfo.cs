6233650df5a21503b3aad773dd50e47a79d48c3a
Rafael Romão <rafaelromao@gmail.com>
2015-09-21 11:13:18 -0300

    configure Nuget

--- ReactiveServices/MessageBus/Properties/AssemblyInfo.cs
+++ ReactiveServices/MessageBus/Properties/AssemblyInfo.cs
@@ -32,9 +32,10 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.2.*")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.0.0")]
+// Theses attributes will be patched by the AppVeyor CI
+[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyInformationalVersion("0.3.0.0")]
 
 
 [assembly: InternalsVisibleTo("ReactiveServices.MessageBus.Tests")]
